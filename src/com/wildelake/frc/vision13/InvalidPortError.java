c969e7f1a6e5979f6cb983cf6dbb6675516c2912
linuxgeek96 <10sdassarma@gmail.com>
2013-01-07 09:01:47 -0500

    Fixed up InvalidPortError

--- src/com/wildelake/frc/vision13/InvalidPortError.java
+++ src/com/wildelake/frc/vision13/InvalidPortError.java
@@ -1,14 +1,14 @@
 package com.wildelake.frc.vision13;
 
 public class InvalidPortError extends Error {
+	
+	private static final long serialVersionUID = 1L;
 
 	public InvalidPortError() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidPortError(String s) {
 		super(s);
-		// TODO Auto-generated constructor stub
 	}
 
 }
